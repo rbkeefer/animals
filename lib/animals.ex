16ce316807bd9be48dc581268ec431d03d84b5aa
Rob Keefer <rob.keefer@pomiet.com>
2017-10-27 20:37:56 -0400

    first commit

---
+++ lib/animals.ex
@@ -0,0 +1,18 @@
+defmodule Animals do
+  @moduledoc """
+  Documentation for Animals.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Animals.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/animals_test.exs
@@ -0,0 +1,8 @@
+defmodule AnimalsTest do
+  use ExUnit.Case
+  doctest Animals
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
