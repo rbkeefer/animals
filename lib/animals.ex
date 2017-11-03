52b70386f63f4877d649ddbc1e735b329c9c3dec
Rob Keefer <rob.keefer@pomiet.com>
2017-11-02 23:57:39 -0400

    Cleanup get_guess function and remove a lot of extra code

--- lib/animals.ex
+++ lib/animals.ex
@@ -10,35 +10,23 @@ defmodule Animals do
 
   """
 
-  def get_new_animal_name() do
-    IO.gets "I'm sorry I don't know. What animal were you thinking of? " |> String.trim
-  end
-
-  def get_new_animal_description() do
-    IO.gets "Great thanks. What is a question that is unique to this animal? " |> String.trim
-  end
-
   def try_prompt(prompt) do
-    IO.puts(inspect(prompt))
-    IO.gets "#{prompt}" |> String.trim
+    IO.gets "#{prompt}"
   end
-
 
   def get_guess([]) do
-    animal = get_new_animal_name()
-    description = get_new_animal_description()
-    # animal = "cat"
-    # description = "Does it ignore you?"
+    animal = (IO.gets "I'm sorry I don't know. What animal were you thinking of? ") |> String.trim
+    description = (IO.gets "Great thanks. What is a question that is unique to this animal? ") |> String.trim
 
-    [animal, description, [], []]
+    ["Is it a #{animal}? ", "#{description}? ", [], []]
   end
 
   def get_guess(animal_list) do
-    prompt_answer = IO.gets try_prompt(Enum.at(animal_list, 1))    # Try description
+    prompt_answer = try_prompt(Enum.at(animal_list, 1))    # Try description
     # prompt_answer = "no"
-    if prompt_answer == "yes" do
-      prompt_answer = IO.gets try_prompt(List.first(animal_list))  # Try anaimal name
-      if prompt_answer == "yes" do
+    if "yes" == String.trim(prompt_answer) do
+      prompt_answer = try_prompt(List.first(animal_list))  # Try anaimal name
+      if "yes" == String.trim(prompt_answer) do
         IO.puts "I knew it!"
       else
         # Go down 'yes' path since it does match description
@@ -49,14 +37,5 @@ defmodule Animals do
       [Enum.at(animal_list, 0), Enum.at(animal_list, 1), Enum.at(animal_list, 2), get_guess(Enum.at(animal_list, 3))]
     end
   end
-
-
-  # def add_new_animal_to_list(animal, descriptive_question) do
-  #   animal_list = [animal, descriptive_question, [], []]
-  # end
-  #
-  # def add_new_animal_to_list(animal, descriptive_question, no_list, yes_list) do
-  #   animal_list = [animal, descriptive_question, no_list, yes_list]
-  # end
 
 end
--- test/animals_test.exs
+++ test/animals_test.exs
@@ -2,20 +2,6 @@ defmodule AnimalsTest do
   use ExUnit.Case
   doctest Animals
 
-  test "Add one animal to the list" do
-    animal_list = Animals.add_new_animal_to_list("frog", "Is it green?")
-    IO.puts(inspect(animal_list))
-
-    assert "frog" == List.first(animal_list)
-  end
-
-  test "Add two animals to the list" do
-    animal_list = Animals.add_new_animal_to_list("frog", "Is it green?", Animals.add_new_animal_to_list("cat", "Does it ignore you?"), [])
-    IO.puts(inspect(animal_list))
-
-    assert "frog" == List.first(animal_list)
-  end
-
   test "Prompt user for new animal / description" do
     animal_list = Animals.get_guess([])
     assert "cat" == List.first(animal_list)
