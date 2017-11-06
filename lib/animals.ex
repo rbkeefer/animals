a75f25ad65ce09dd7222c1e10ece543a2158816d
Rob Keefer <rob.keefer@pomiet.com>
2017-11-06 17:15:20 -0500

    Support Unit Tests The unit tests broke when user interaction was introduced. Separated out the interaction and created a FakeIO to support the unit tests.

--- lib/animals.ex
+++ lib/animals.ex
@@ -10,41 +10,52 @@ defmodule Animals do
 
   """
 
-  def prompt(prompt) do
-    IO.gets "#{prompt}"
+  # List of prompts and responses used to interact with the user
+  @dont_know "I'm sorry I don't know. What animal were you thinking of? "
+  @get_unique_question "Great thanks. What is a question that is unique to this animal? "
+  @thanks "\nThank you. I like to learn these things.\n"
+  @knew_it "\nI knew it!"
+  @instructions "\n\n\n\nThink of an animal and I'll try to figure it out by asking you questions.\n\n"
+  @ready "Ready? "
+
+  def prompt(message, io \\ IO) do
+    io.gets "#{message}"
   end
 
-  def guess([]) do
-    animal = (IO.gets "I'm sorry I don't know. What animal were you thinking of? ") |> String.trim
-    description = (IO.gets "Great thanks. What is a question that is unique to this animal? ") |> String.trim
+  def response(message, io \\ IO) do
+    io.puts "#{message}"
+  end
 
-    IO.puts "\nThank you. I like to learn these things.\n"
+  def guess([], io) do
+    animal = (prompt(@dont_know, io)) |> String.trim
+    description = (prompt(@get_unique_question, io)) |> String.trim
+
+    response(@thanks, io)
 
     ["Is it a #{animal}? ", "#{description}? ", [], []]
   end
 
-  def guess(animal_list) do
-    prompt_answer = prompt(Enum.at(animal_list, 1))    # Try description
+  def guess(animal_list, io) do
+    prompt_answer = prompt(Enum.at(animal_list, 1), io)    # Try description
     # prompt_answer = "no"
     if "yes" == String.trim(prompt_answer) do
-      prompt_answer = prompt(List.first(animal_list))  # Try anaimal name
+      prompt_answer = prompt(List.first(animal_list), io)  # Try anaimal name
       if "yes" == String.trim(prompt_answer) do
-        IO.puts "\nI knew it!"
+        response(@knew_it, io)
       else
         # Go down 'yes' path since it does match description
-        [Enum.at(animal_list, 0), Enum.at(animal_list, 1), guess(Enum.at(animal_list, 2)), Enum.at(animal_list, 3)]
+        [Enum.at(animal_list, 0), Enum.at(animal_list, 1), guess(Enum.at(animal_list, 2), io), Enum.at(animal_list, 3)]
       end
     else
       # Go down 'no' path since the description didn't match
-      [Enum.at(animal_list, 0), Enum.at(animal_list, 1), Enum.at(animal_list, 2), guess(Enum.at(animal_list, 3))]
+      [Enum.at(animal_list, 0), Enum.at(animal_list, 1), Enum.at(animal_list, 2), guess(Enum.at(animal_list, 3), io)]
     end
   end
 
   def start(animal_list) do
-    IO.puts "\n\n\n\nThink of an animal and I'll try to figure it out by asking you questions.\n\n"
-    IO.gets "Ready? "
-    IO.puts "\n"
-    start(guess(animal_list))
+    response(@instructions)
+    prompt(@ready)
+    start(guess(animal_list, IO))
   end
 
   def main(args \\ []) do
--- test/animals_test.exs
+++ test/animals_test.exs
@@ -1,15 +1,29 @@
+
+defmodule FakeIO do
+  @dont_know "I'm sorry I don't know. What animal were you thinking of? "
+  @get_unique_question "Great thanks. What is a question that is unique to this animal? "
+  @get_description "Is it green? "
+  @get_animal_name "Is it a frog? "
+  defdelegate puts(message), to: IO
+
+  def gets(@dont_know), do: "frog"
+  def gets(@get_unique_question), do: "Is it green"
+  def gets(@get_description), do: "yes"
+  def gets(@get_animal_name), do: "yes"
+  def gets(value), do: raise ArgumentError, message: "invalid argument #{value}"
+end
+
 defmodule AnimalsTest do
   use ExUnit.Case
   doctest Animals
 
   test "Prompt user for new animal / description" do
-    animal_list = Animals.get_guess([])
-    assert "cat" == List.first(animal_list)
+    animal_list = Animals.guess([], FakeIO)
+    assert "Is it a frog? " == List.first(animal_list)
   end
 
   test "Add second animal to list" do
-    animal_list = Animals.add_new_animal_to_list("frog", "Is it green?")
-    assert :ok == Animals.get_guess(animal_list)
+    assert :ok = Animals.guess(["Is it a frog? ", "Is it green? ", [], []], FakeIO)
   end
 
 end
